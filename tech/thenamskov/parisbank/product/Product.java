c474048991cef455f582197e36a9197eba87d8ae
theNamskov <41073590+theNamskov@users.noreply.github.com>
2021-10-12 15:06:58 +0000

    feat: added base pricing impl for products.

--- tech/thenamskov/parisbank/product/Product.java
+++ tech/thenamskov/parisbank/product/Product.java
@@ -1,6 +1,20 @@
 package tech.thenamskov.parisbank.product;
 
 
-public abstract class Product {
+import tech.thenamskov.parisbank.pricing.*;
+
+public abstract class Product implements ProductPricingService {
   
+  
+  double price(String exchange, String ticker) {
+    throw new NotImplementedException("Not allowed for this product.");
+  }
+
+  double price(String exchange, String contractCode, int month, int year) {
+    throw new NotImplementedException("Not allowed for this product.");
+  }
+
+  double price(String exchange, String ticker, int validForDays) {
+    throw new NotImplementedException("Not allowed for this product.");
+  }
 }
